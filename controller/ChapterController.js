5f5418d4eff9df77ba91593961686eeeab83be68
Christoph Forster <christoph.forster@gmail.com>
2015-11-06 06:23:26 -0800

    Model added

--- controller/ChapterController.js
+++ controller/ChapterController.js
@@ -1,6 +1,6 @@
 /**
  * Created by devebiz on 11/6/15.
  */
-app.controller("ChapterController", function ($scope) {
-   $scope.info = "The Controller";
+app.controller("ChapterController", function ($scope, ChapterModel) {
+   $scope.info = ChapterModel.getChapters();
 });
--- model/ChapterModel.js
+++ model/ChapterModel.js
@@ -1,3 +1,33 @@
 /**
  * Created by devebiz on 11/6/15.
  */
+
+(function () {
+    var ChapterModel = function () {};
+
+    Object.defineProperties(ChapterModel.prototype, {
+        getChapters : {
+            value: function() {
+                return [
+                {
+                    id: 989,
+                        title: "Überschrift 1",
+                    summary:"Zusammenfassung"
+                },
+                {
+                    id: 989,
+                        title: "Überschrift 1",
+                    summary:"Zusammenfassung"
+                },
+                {
+                    id: 989,
+                        title: "Überschrift 1",
+                    summary:"Zusammenfassung"
+                }
+            ]
+        }
+    }
+});
+app.service("ChapterModel",ChapterModel);
+
+}());
